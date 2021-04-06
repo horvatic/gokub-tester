22167498194d6a294dbad6f664d9e03be96e6b31
horvatic <sbhorvatic@gmail.com>
2021-04-06 03:05:24 +0000

    add dev deploy

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # gokub-tester
 A project to be deployed, and test kubernetes deployment. 
+
+## Using
+Run the following command to deploy
+```
+kubectl apply -k deploy/dev
+```
---
+++ cmd/gokub-tester/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
+
+func main() {
+	namespace := os.Getenv("NAMESPACE")
+	service := os.Getenv("SERVICE")
+	http.HandleFunc(fmt.Sprintf("/%s/%s/%s", namespace, service, "health"), health)
+	http.HandleFunc("/", testReq)
+	http.ListenAndServe(":8080", nil)
+}
+
+func testReq(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "echo:, %s!", r.URL.Path[1:])
+}
+
+func health(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(200)
+	w.Write([]byte("ok"))
+}
