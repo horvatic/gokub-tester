4c049f7d2922a52e370c0f249b025c9049b063d6
horvatic <sbhorvatic@gmail.com>
2021-04-07 03:40:44 +0000

    fix health endpoint

--- cmd/gokub-tester/main.go
+++ cmd/gokub-tester/main.go
@@ -3,25 +3,11 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"time"
 )
 
 func main() {
-
-	started := time.Now()
-
-	http.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
-		duration := time.Since(started)
-		if duration.Seconds() > 10 {
-			w.WriteHeader(500)
-			w.Write([]byte(fmt.Sprintf("error: %v", duration.Seconds())))
-		} else {
-			w.WriteHeader(200)
-			w.Write([]byte("ok"))
-		}
-	})
-
 	http.HandleFunc("/gokub/health", health)
+	http.HandleFunc("/health", health)
 	http.HandleFunc("/", testReq)
 	http.ListenAndServe(":8080", nil)
 }
